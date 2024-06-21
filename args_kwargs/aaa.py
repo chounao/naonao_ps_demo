9c4d650e2022a46e79fbf3ad9a8c6cf44804d6a9
“zhou112517@gmail.com” <zhou112517@gmail.com>
2024-06-21 16:39:11 +0800

    Initial commit

--- args_kwargs/aaa.py
+++ args_kwargs/aaa.py
@@ -1,70 +1,50 @@
 import requests
 import pandas as pd
 import os
-import openpyxl
-# 读取 Excel 文件
-# s=requests.session()
-# path = 'C:/Users/86185/Desktop/周超杰/mimt/args_kwargs/到期未续费未登录用户.xlsx'
-# sheet_name = 'Sheet1'
-# df = pd.read_excel(path,sheet_name)
-# url = 'https://mgr.xyy001.com/qnfhdcs/base/queryBaseInfo.do'
-#
-# h = {
-# 'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8'
-# }
-# s.headers.update(h)
-# phone_list = []
-# for i in df['sellerId']:
-#     data = {
-#         'sellerNick':int(i),
-#         'token':'VmQBMVExX2gHYVYuUSNaRVE1UiYFb1NvAHBdRQdqBXAKTFdvBnJfHQNhBXRYN1A%2FVSFXQwNmAytUOAgVAnJURlZlAS1RZl8%2BBz9WcVFjWixRZlJiBTpTLQAwXSgHYgV5CiVXZwZ7X3MDZgVmWD5QMlU9VzMDZAM9VDkIaA%3D%3D'}
-#     r = s.post(url,data)
-#     data = r.json()
-#     if 'sellerInfo' in data['data']:
-#         phone = data['data']['sellerInfo']['cellphone']
-#         phone_list.append(phone)
-#     else:
-#         phone_list.append('N/A')
-#
-# df['phone'] = phone_list
-#
-# with pd.ExcelWriter(path,engine='openpyxl')as writer:
-#     df.to_excel(writer,sheet_name=sheet_name,index=False)
 
 
-def read():
-    path = os.getcwd()+'/到期未续费未登录用户.xlsx'
-    df = pd.read_excel(path, sheet_name = 'Sheet1')
-    return df
 
-def get_phone(token):
-    s = requests.session()
-    url = 'https://mgr.xyy001.com/qnfhdcs/base/queryBaseInfo.do'
-    h = {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}
-    s.headers.update(h)
-    phone_list = []
-    for i in read()['sellerId']:
-        data = {
-            'sellerNick': int(i),
-            'token':token
-        }
-        r = s.post(url, data)
-        data = r.json()
-        if 'sellerInfo' in data['data']:
-            phone = data['data']['sellerInfo']['cellphone']
-            phone_list.append(phone)
-        else:
-            phone_list.append('N/A')
-    return phone_list
-def save_excel(token):
-    df = read()
-    df['phone'] = get_phone(token)
-    
-    with pd.ExcelWriter(path, engine='openpyxl') as writer:
-        df.to_excel(writer, sheet_name='Sheet1', index=False)
+def read(path):
+	df = pd.read_excel(path, sheet_name='Sheet1')
+	return df
+
+
+def get_phone(token,path):
+	s = requests.session()
+	url = 'https://mgr.xyy001.com/qnfhdcs/base/queryBaseInfo.do'
+	h = {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}
+	s.headers.update(h)
+	phone_list = []
+	for i in read(path)['sellerId']:
+		if type(i) == str:
+			pass
+		else:
+			i = int(i)
+		data = {
+			'sellerNick': i,
+			'token': token
+		}
+		r = s.post(url, data)
+		data = r.json()
+		print(data)
+		if 'sellerInfo' in data['data']:
+			phone = data['data']['sellerInfo']['cellphone']
+			phone_list.append(phone)
+		else:
+			phone_list.append('N/A')
+	return phone_list
+
+
+def save_excel(token,path):
+	df = read(path)
+	df['phone'] = get_phone(token,path)
+	with pd.ExcelWriter(path, engine='openpyxl') as writer:
+		df .to_excel(writer, sheet_name='Sheet1', index=False)
+
+
 
 
 if __name__ == '__main__':
     token ='VmQBMVExX2gHYVYuUSNaRVE1UiYFb1NvAHBdRQdqBXAKTFdvBnJfHQNhBXRYN1A%2FVSFXQwNmAytUOAgVAnJURlZlAS1RZl8%2BBz9WcVFjWixRZlJiBTpTLQAwXSgHYgV5CiVXZwZ7X3MDZgVmWD5QMlU9VzMDZAM9VDkIaA%3D%3D'
-    path = 'C:/Users/86185/Desktop/周超杰/mimt/args_kwargs/到期未续费未登录用户.xlsx'
+    path = os.getcwd() + '/到期未续费未登录用户.xlsx'
     save_excel(token)
